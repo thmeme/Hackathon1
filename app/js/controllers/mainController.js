4da57ab6d8affc68f52fdc3cdc30e0800ade3de0
thmeme <thierrymeme@gmail.com>
2017-03-23 17:14:38 +0100

    initialize ng-map

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,4 +1,4 @@
 angular.module('app')
-    .controller('MainController', function($scope) {
+    .controller('MainController', function($scope, NgMap) {
       /* Here is your main controller */
     });
